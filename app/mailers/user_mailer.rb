94df473ff95c56390b16a2ac6cf9b2ce40447af8
ahmed hanafy <ahr.mahmoud@gmail.com>
2017-04-08 23:44:06 +0200

    mailgun2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   # with the following lookup:
   #
   #   en.user_mailer.signup_confirmation.subject
-  default from: 'postmaster@app.campaignpmp.com'
+  default from: ENV['MAILGUN_SMTP_LOGIN']
 
   
   def signup_confirmation(user)
