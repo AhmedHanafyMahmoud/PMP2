349950905eca23883212ab0f0f59cf9ffe72e300
ahmed hanafy <ahr.mahmoud@gmail.com>
2017-04-08 23:35:40 +0200

    mailgun2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-      api_key: ENV['mailgun_API'],
-      domain: ENV['mailgun_domain']
+      api_key: ENV['MAILGUN_API_KEY'],
+      domain: ENV['MAILGUN_DOMAIN']
   }
   config.action_mailer.perform_deliveries = true
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
-      api_key: ENV['mailgun_API'],
-      domain: ENV['mailgun_domain']
+      api_key: ENV['MAILGUN_API_KEY'],
+      domain: ENV['MAILGUN_DOMAIN']
   }
   config.action_mailer.perform_deliveries = true
   # Code is not reloaded between requests.
