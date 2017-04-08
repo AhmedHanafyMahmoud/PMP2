2b762748af8a1b639c43ffc0b15d30a67ffed8b9
ahmed hanafy <ahr.mahmoud@gmail.com>
2017-04-08 21:07:27 +0200

    mailgun2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://pmp2.herokuapp.com'}
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address:              'smtp.gmail.com',
