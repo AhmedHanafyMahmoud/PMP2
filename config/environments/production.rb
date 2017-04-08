97275902292514714b28cb762b6e53b5d99981db
ahmed hanafy <ahr.mahmoud@gmail.com>
2017-04-08 22:44:54 +0200

    mailgun2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+require 'mailgun'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://pmp2.herokuapp.com'}
